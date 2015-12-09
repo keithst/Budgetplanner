11802134e6608354b99b46d12093a74be64bf5f9
keithst <keith.sturzenbecker@gmail.com>
2015-12-09 15:35:28 -0500

    Fixed registration

--- Budgetplanner/Controllers/AccountController.cs
+++ Budgetplanner/Controllers/AccountController.cs
@@ -328,7 +328,7 @@ namespace Budgetplanner.Controllers
                 return BadRequest(ModelState);
             }
 
-            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email };
+            var user = new ApplicationUser() { UserName = model.Email, Email = model.Email, fname = model.FirstName, lname = model.LastName };
 
             IdentityResult result = await UserManager.CreateAsync(user, model.Password);
 
